0504201cd1dcd175780e51afa831f9327a3597d0
Folareen <sakawahab03@gmail.com>
2022-04-07 13:11:43 +0100

    done with generate password functionality

--- home.js
+++ home.js
@@ -1,8 +1,40 @@
-// select
-    // length
-    // setttings... upper lower number symbols
-// 
+const PASSWORD = document.querySelector('.password');
+const COPY = document.querySelector('generated-password button');
+const PASSWORD_LENGTH = document.getElementById('length');
+const UPPERCASE = document.getElementById('uppercase');
+const LOWERCASE = document.getElementById('lowercase');
+const NUMBERS = document.getElementById('numbers');
+const SYMBOLS = document.getElementById('symbols');
+const GENERATE_PASSWORD_BUTTON = document.getElementById('generate');
+const GENERATE_EASY_PASSWORD_BUTTON = document.getElementById('generate-easy');
+
+const UPPERCASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
+const NUMBERS_CHARS = "1234567890";
+const SYMBOLS_CHARS = "`~!@#$%^&*()_-+={[}]|\\;'\"";
 
 
 
-const INCLUDE_UPPER = document.querySelector()
+function include(chars, checkBox){
+    let random = Math.random();
+    let char;
+    if(checkBox.checked){
+        char = chars[Math.floor(random * chars.length)];
+        return char;
+    }
+    else{
+        return "";
+    }
+}
+
+GENERATE_PASSWORD_BUTTON.addEventListener('click', generatePassword);
+
+function generatePassword(){
+    let generatedPassword = "";
+    for(let i = 0; i < PASSWORD_LENGTH.value; i+=4){
+        generatedPassword += include(UPPERCASE_CHARS, UPPERCASE) + include(LOWERCASE_CHARS, LOWERCASE) + include(NUMBERS_CHARS, NUMBERS) + include(SYMBOLS_CHARS, SYMBOLS);
+    }
+    let password = generatedPassword.slice(0, PASSWORD_LENGTH.value);
+    PASSWORD.innerText = password;
+}
+
